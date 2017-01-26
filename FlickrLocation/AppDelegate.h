33dd1d89dd2e738a93ac72d16656e8fd8a3d4d8b
iUS <ius@iuss-macbook-pro.local>
2017-01-25 19:48:39 -0800

    Initial Commit

---
+++ FlickrLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrLocation
+//
+//  Created by iUS on 1/25/17.
+//  Copyright © 2017 FlickrLocation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrLocation
+//
+//  Created by iUS on 1/25/17.
+//  Copyright © 2017 FlickrLocation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
